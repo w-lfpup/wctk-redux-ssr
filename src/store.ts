661b5951896f9d8c040258072e277a5c1fe41f03
Taylor Vann <brian.t.vann@gmail.com>
2025-05-10 17:46:46 -0700

    shape store

---
+++ src/store.ts
@@ -0,0 +1,66 @@
+/*
+  store.ts
+
+  This module creates and exports a redux store. It uses
+  one reducer to account for a list of circles and squares.
+
+  This is a vanilla redux store. Redux does NOT need any
+  modifications to work alongside Lit or webcomponents.
+*/
+
+import type { Reducer, Action } from '@reduxjs/toolkit';
+
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+type Shape = 'square' | 'circle';
+type RemoveShape = (state: Shape[], shape: Shape) => void;
+
+interface State {
+	circles: number;
+	squares: number;
+	shapeList: Shape[];
+}
+
+const initialState: State = { squares: 0, circles: 0, shapeList: [] };
+
+const removeShape: RemoveShape = (shapes: Shape[], shape: Shape) => {
+	const index = shapes.lastIndexOf(shape);
+	if (index > -1) {
+		shapes.splice(index, 1);
+	}
+};
+
+const shapeSlice = createSlice({
+	name: 'shapes',
+	initialState,
+	reducers: {
+		reset: state => { 
+			state.circles = 0;
+			state.squares = 0;
+			state.shapeList = [];
+		},
+		increment_squares: state => {
+			state.squares += 1;
+			state.shapeList.push('square');
+		},
+		decrement_squares: state => {
+			state.squares -= 1;
+			removeShape(state.shapeList, 'square');
+		},
+		increment_circles: state => {
+			state.circles += 1;
+			state.shapeList.push('circle');
+		},
+		decrement_circles: state => {
+			state.circles += 1;
+			removeShape(state.shapeList, 'circle');
+		},
+	}
+});
+
+const store = configureStore({
+	reducer: shapeSlice.reducer
+});
+
+export type { State, Shape };
+export { store };
